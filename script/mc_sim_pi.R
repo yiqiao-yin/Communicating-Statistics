ecb7f93f8756e372dd4994600e70b5b2ec910a6f
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2019-03-28 18:13:47 -0400

    Update mc_sim_pi.R

--- script/mc_sim_pi.R
+++ script/mc_sim_pi.R
@@ -7,12 +7,15 @@ piR.plot <- function(N) {
   y <- runif(N)
   d <- sqrt(x^2 + y^2)
   label <- ifelse(d < 1, 1, 0)
-  plot(x,y,col=label+1,main=paste0("Simulation of Pi Using N=",N),
+  plot(x,y,col=label+1,
+       main=paste0("Simulation of Pi: N=",N,
+                   "; Simulated value=",
+                   round(4*plyr::count(label)[2,2]/N,3)),
        pch = 20, cex = 1)
 }
 
 ## Plot Monte Carlo Simulation of Pi
 saveGIF({
-  for (i in c(100,200,500,1000,2000,5000,10000,15000,20000)) {piR.plot(i)}
-}, movie.name = "mc-sim-pi.gif", interval = 0.3, nmax = 30, 
+  for (i in c(10,1e2,1e3,1e4,1e5,1e6)) {piR.plot(i)}
+}, movie.name = "mc-sim-pi.gif", interval = 0.8, nmax = 30, 
 ani.width = 480)
