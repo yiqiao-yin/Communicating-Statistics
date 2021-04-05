396e1bfbeab51154458b6d0f117f33aeadf9ebc1
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-04-05 11:25:42 -0400

    Update mc_sim_random_walk.R

--- script/mc_sim_random_walk.R
+++ script/mc_sim_random_walk.R
@@ -1,4 +1,3 @@
-
 # Library
 library(animation)
 
@@ -6,19 +5,25 @@ library(animation)
 num.of.sim <- 50
 num.of.days <- 200
 data <- matrix(rnorm(num.of.sim*num.of.days,mean=0,sd=0.01),nrow=num.of.days)
+data[1, ] = 0L
 
 # Create GIF
+setwd("C:/Users/eagle/OneDrive/Desktop/")
 saveGIF({
-  for (N in seq(10,200,10)) {
+  for (N in seq(10,num.of.days,10)) {
     select.data <- data[1:N, ]
     cumret <- select.data + 1L
     cumretpath <- apply(cbind(cumret), 2, cumprod)
     plot(x = 1:N, y = cumretpath[,1], type = "l", 
-         ylab = "Simulated Path for $1 Investment",
+         main = paste0(
+           "Simulated Path for $1 Investment\n Comment: X1, X2, ..., X", num.of.sim, 
+           " drawn from N(0,0.01) assuming iid"),
+         ylab = "Numbers in USD",
          xlab = paste0("Time from Day 1 to Day ", N), 
-         col = 1, xlim = c(10, 200), ylim = c(min(cumretpath), max(cumretpath)))
+         xaxs = "i", yaxs = "i",
+         col = 1, xlim = c(1, num.of.days), ylim = c(min(cumretpath), max(cumretpath)))
     for (i in 1:num.of.sim) { lines(x = 1:N, y = cumretpath[, i], type = "l", col = i) }
   }
-}, movie.name = "mc-sim-random-walk.gif", interval = 0.3, nmax = 30, 
-ani.width = 480)
-
+}, movie.name = "mc-sim-random-walk.gif", interval = 1, nmax = 30,
+# convert = "magick", 
+ani.width = 600)
